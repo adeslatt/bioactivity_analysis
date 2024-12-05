707d2a735596e4c70a3e596c54157c7cffec1be6
adeslatt <adeslat@scitechcon.org>
2024-12-05 09:30:46 -0500

    cleaning

--- notebook_detect.py
+++
@@ -1,9 +0,0 @@
-def is_notebook():
-    """
-    Detect if the code is running in a Jupyter Notebook.
-    """
-    try:
-        shell = get_ipython().__class__.__name__
-        return shell == "ZMQInteractiveShell"
-    except NameError:
-        return False
